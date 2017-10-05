ac90e897d1ba98a7c06babfab0ebf0a1312e0b8c
kevin-school <zkwandwyx141@gmail.com>
2017-10-05 14:00:21 -0400

    livehacks1

---
+++ src/unit1/livehacks1/LiveHackProblem1.java
@@ -0,0 +1,15 @@
+package unit1.livehacks1;
+import codehs.*;
+
+/**
+ * A simple program
+ * @author: E. Fabroa
+ *
+ */
+public class LiveHackProblem1 extends ConsoleProgram{
+
+    public void run() {
+        System.out.println("This is my first program");
+
+    }
+}
---
+++ src/unit1/livehacks1/LiveHackProblem2.java
@@ -0,0 +1,4 @@
+package unit1.livehacks1;
+
+public class LiveHackProblem2 {
+}
