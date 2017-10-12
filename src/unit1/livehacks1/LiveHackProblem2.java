a92c4001bab98fba9ed512847c81b726bb5493c1
Yuxuan-Wu141 <31933192+Yuxuan-Wu141@users.noreply.github.com>
2017-10-12 14:19:09 -0400

    Update LiveHackProblem2.java

--- src/unit1/livehacks1/LiveHackProblem2.java
+++ src/unit1/livehacks1/LiveHackProblem2.java
@@ -7,10 +7,12 @@ import codehs.*;
  *
  */
 public class LiveHackProblem2 extends ConsoleProgram{
+      
       public void run{
       double c = readDouble("the temperature in celsius is ");
       double f = 9 * c / 5 + 32;
       System.out.print("The temperature in fahrenheit is " + f);
       System.out.println(" degrees");
+            
       }
 }
